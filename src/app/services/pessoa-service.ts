529b313f800d253772275000eb3ceae09907963a
Vinicius <vpburci@gmail.com>
2025-10-15 12:03:36 -0300

    criando pessoa-service inicial

---
+++ src/app/services/pessoa-service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { PessoaFormData } from '../entities/pessoaFormaData.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoaService {
+  private baseUrl = '/api/v1/pessoa';
+
+  constructor(private http: HttpClient) {}
+
+  buscarTodasPessoas(): Observable<PessoaFormData[]> {
+    return this.http.get<PessoaFormData[]>(this.baseUrl);
+  }
+
+  buscarPessoa(pessoaId: number): Observable<PessoaFormData> {
+    return this.http.get<PessoaFormData>(`${this.baseUrl}/${pessoaId}`);
+  }
+
+  criarPessoa(pessoa: PessoaFormData): Observable<PessoaFormData> {
+    return this.http.post<PessoaFormData>(this.baseUrl, pessoa);
+  }
+
+  atualizarPessoa(id: number, pessoa: PessoaFormData): Observable<PessoaFormData> {
+    return this.http.put<PessoaFormData>(`${this.baseUrl}/atualizar/${id}`, pessoa);
+  }
+
+  deletarPessoa(pessoaId: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${pessoaId}`);
+  }
+}
