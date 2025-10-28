0c6442e12a02729e784f3df3389af87b63f40f7c
Vinicius <vpburci@gmail.com>
2025-10-28 17:56:35 -0300

    consertando DateDisplayPipe calculando data em UTC sem ser afetado por timezone

--- src/app/pipes/date-display-pipe.ts
+++ src/app/pipes/date-display-pipe.ts
@@ -8,10 +8,10 @@ export class DateDisplayPipe implements PipeTransform {
   transform(value: string | null | undefined): string {
     if (!value) return '';
 
-    const date = new Date(value); // Cria um objeto Date a partir da string
-    const day = String(date.getDate()).padStart(2, '0'); // Adiciona o zero à esquerda
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Adiciona o zero à esquerda (meses começam de 0)
-    const year = date.getFullYear();
+    const date = new Date(value);
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const year = date.getUTCFullYear();
 
     return `${day}/${month}/${year}`;
   }
