6b9f47859022ac5e0566d39d5827fe4bd683caef
Vinicius <vpburci@gmail.com>
2025-11-03 11:35:13 -0300

    adicionando formulario CadastroRecursoCelular

--- src/app/recursos/recurso-celular/cadastro-recurso-celular/cadastro-recurso-celular.ts
+++ src/app/recursos/recurso-celular/cadastro-recurso-celular/cadastro-recurso-celular.ts
@@ -1,10 +1,16 @@
 import { AfterViewInit, ChangeDetectorRef, Component, inject, input, OnInit, output, signal } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CelularService } from '../../../services/celular-service';
 import { RecursoCelularRequestDTO } from '../../../entities/recursoCelularRequestDTO.model';
+import { RecursoCelularResponseDTO } from '../../../entities/recursoCelularResponseDTO.model';
+import { CelularFormData } from '../../../entities/celularFormData.model';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { DataService } from '../../../services/data-service';
 
 @Component({
   selector: 'app-cadastro-recurso-celular',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgxMaskDirective],
+  providers: [provideNgxMask()],
   templateUrl: './cadastro-recurso-celular.html',
   styleUrl: './cadastro-recurso-celular.css'
 })
@@ -12,9 +18,12 @@ export class CadastroRecursoCelular implements OnInit, AfterViewInit {
 
   private fb = inject(FormBuilder);
   private cdRef = inject(ChangeDetectorRef);
+  private celularService = inject(CelularService);
+  private dataService = inject(DataService);
 
   editMode = signal<boolean>(false);
   pessoaId = input<string | null>(null);
+  listaCelulares = signal<CelularFormData[] | null>(null);
   closeForm = output<void>();
 
   form!: FormGroup<{ [K in keyof RecursoCelularRequestDTO]: FormControl<RecursoCelularRequestDTO[K]> }>;
@@ -26,10 +35,16 @@ export class CadastroRecursoCelular implements OnInit, AfterViewInit {
       dataEntrega: new FormControl<string | null>(null),
       dataDevolucao: new FormControl<string | null>(null),
     });
+    this.celularService.listar().subscribe(data => this.listaCelulares.set(data));
   }
 
   onSubmit() {
-    console.log(this.form.value);
+    console.log({...this.form.value,
+        pessoaId: this.pessoaId(), 
+        dataEntrega: this.dataService.convertDateToISO(this.form.value.dataEntrega!),
+        dataDevolucao: this.dataService.convertDateToISO(this.form.value.dataDevolucao!)
+      } as RecursoCelularRequestDTO);
+
   }
 
   onCloseForm() {
@@ -37,6 +52,6 @@ export class CadastroRecursoCelular implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.cdRef.detectChanges();
+    setTimeout(() => this.cdRef.detectChanges());
   }
 }
